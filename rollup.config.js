c2834c55f6f2466654f1d4d7fbdd0a6631df1a61
FedericoCarboni <fr3ddydev@gmail.com>
2020-09-21 22:51:46 +0200

    build: update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,33 @@
 import typescript from '@rollup/plugin-typescript';
+import { execSync } from 'child_process';
+import pkg from './package.json';
+
+const commit = execSync('git rev-parse HEAD').toString('utf-8').slice(0, -1);
+
+const banner = `/**
+ * ${pkg.name} v${pkg.version}
+ * ${new Date().toUTCString()}
+ * Commit ${commit}
+ * ${pkg.homepage}
+ * @author ${pkg.author.name}
+ * @license ${pkg.license}
+ */`;
 
 export default [{
   input: 'src/lib.ts',
   output: [{
     entryFileNames: 'lib.es6.js',
     format: 'es',
-    dir: 'lib'
+    dir: 'lib',
+    banner,
   }, {
     entryFileNames: 'lib.cjs',
     format: 'cjs',
-    dir: 'lib'
+    dir: 'lib',
+    banner,
   }],
   plugins: [
-    typescript(),
-  ]
+    typescript({ tsconfig: './tsconfig.es6.json' }),
+  ],
+  external: ['child_process', 'path', 'fs'],
 }];
