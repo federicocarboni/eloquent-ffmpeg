f8f566fe799bd6e5e17d0ab426ad9080f7382977
FedericoCarboni <fr3ddydev@gmail.com>
2020-09-22 08:29:03 +0200

    test: added tests for env.ts

---
+++ test/env_test.ts
@@ -0,0 +1,26 @@
+import { getFFmpegPath, getFFprobePath, setFFmpegPath, setFFprobePath } from '../src/env';
+import { expect } from 'chai';
+
+describe('env', function () {
+  describe('setFFmpegPath()', function () {
+    it('throws on an invalid path', function () {
+      expect(() => setFFmpegPath('broken/:path')).to.throw(TypeError);
+    });
+  });
+  describe('setFFprobePath()', function () {
+    it('throws on an invalid path', function () {
+      expect(() => setFFprobePath('broken/:path')).to.throw(TypeError);
+    });
+  });
+
+  describe('getFFmpegPath()', function () {
+    it('returns a string', function () {
+      expect(getFFmpegPath()).to.be.a('string');
+    });
+  });
+  describe('getFFprobePath()', function () {
+    it('throws on an invalid path', function () {
+      expect(getFFprobePath()).to.be.a('string');
+    });
+  });
+});
