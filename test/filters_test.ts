ebd5dfa2ffdf4cf14c431889b5acb98c2bab75a6
FedericoCarboni <fr3ddydev@gmail.com>
2020-09-26 21:04:24 +0200

    test: update tests

--- test/filters_test.ts
+++ test/filters_test.ts
@@ -3,10 +3,14 @@ import { expect } from 'chai';
 
 describe('filters', function () {
   describe('escapeFilterComponent()', function () {
-    it('should escape invalid characters', function () {
+    it('should escape special characters', function () {
       const unescaped = "I'm a string;, with many[special]: characters";
       const escaped = "I\\'m a string\\;\\, with many\\[special\\]\\: characters";
       expect(escapeFilterComponent(unescaped)).equals(escaped);
+    });
+    it('should escape special characters (non-string)', function () {
+      // @ts-expect-error
+      expect(escapeFilterComponent({})).equals('\\[object Object\\]');
     });
   });
   describe('stringifySimpleFilter()', function () {
@@ -17,6 +21,10 @@ describe('filters', function () {
     it('should stringify options record', function () {
       expect(stringifySimpleFilter('my_filter', { opt1: 'val1', opt2: 'val2' })).equals('my_filter=opt1=val1:opt2=val2');
       expect(stringifySimpleFilter('my_filter', { opt1: 'val1:', opt2: 'val2' })).equals('my_filter=opt1=val1\\::opt2=val2');
+    });
+    it('should stringify non-string values', function () {
+      expect(stringifySimpleFilter('my_filter', [1, 2])).equals('my_filter=1:2');
+      expect(stringifySimpleFilter('my_filter', { opt1: 1, opt2: 2 })).equals('my_filter=opt1=1:opt2=2');
     });
     it('should stringify empty options', function () {
       expect(stringifySimpleFilter('my_filter', [])).equals('my_filter');
