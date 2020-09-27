3e427e60fa0fd5821a8af6e59b603d8479e52909
FedericoCarboni <fr3ddydev@gmail.com>
2020-09-27 14:57:14 +0200

    feat: improve type declarations and implementation

--- src/command.ts
+++ src/command.ts
@@ -1,8 +1,11 @@
 import { ChildProcess, ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { BufferLike, end, isBufferLike, isWin32, write } from './utils';
 import { createInterface as readlines } from 'readline';
 import { toUint8Array } from '../lib/utils';
+import { __asyncValues } from 'tslib';
 import { getFFmpegPath } from './env';
-import { BufferLike, isWin32 } from './utils';
+import { getSockPath } from './sock';
+import { Readable } from 'stream';
 
 export enum LogLevel {
   Quiet = 'quiet',
@@ -17,7 +20,7 @@ export enum LogLevel {
 }
 
 export type InputSource = string | BufferLike | AsyncIterable<BufferLike> | Iterable<BufferLike> | NodeJS.ReadableStream;
-export type OutputDestination = string | null | { [Symbol.asyncIterator](): AsyncIterator<any, any, Uint8Array>; } | { [Symbol.iterator](): Iterator<any, any, Uint8Array>; } | NodeJS.WritableStream;
+export type OutputDestination = string | { [Symbol.asyncIterator](): AsyncIterator<any, any, Uint8Array>; } | { [Symbol.iterator](): Iterator<any, any, Uint8Array>; } | NodeJS.WritableStream;
 
 export interface Progress {
   frames: number;
@@ -32,11 +35,13 @@ export interface Progress {
 
 export interface FFmpegInput {
   args(...args: string[]): this;
+  getArgs(): string[];
   isStream: boolean;
 }
 
 export interface FFmpegOutput {
   args(...args: string[]): this;
+  getArgs(): string[];
   isStream: boolean;
 }
 
@@ -62,35 +67,48 @@ export interface FFmpegCommand {
 }
 
 export interface FFmpegOptions {
-  logLevel: LogLevel;
+  logLevel?: LogLevel;
+  progress?: boolean;
 }
 
+export function ffmpeg(options?: FFmpegOptions): FFmpegCommand {
+  return new Command(options);
+}
+
+export const MAX_BUFFER_LENGTH = 16383;
+
 class Command implements FFmpegCommand {
-  #args: string[] = ['-progress', 'pipe:2', '-nostats'];
-  #inputs;
-  #outputs;
+  #args: string[] = ['-y'];
+  #inputs: FFmpegInput[] = [];
+  #outputs: FFmpegOutput[] = [];
 
   private logLevel: LogLevel;
-  constructor(options?: FFmpegOptions) {
-    this.logLevel = options?.logLevel ?? LogLevel.Error;
+  constructor(options: FFmpegOptions = {}) {
+    this.logLevel = options.logLevel ?? LogLevel.Error;
+    if (options.progress !== false)
+      this.#args.push('-progress', 'pipe:2', '-nostats');
   }
   input(source: InputSource): FFmpegInput {
-    throw new Error('Method not implemented.');
+    return new Input(source);
   }
   output(...destinations: OutputDestination[]): FFmpegOutput {
-    throw new Error('Method not implemented.');
+    return new Output(destinations);
   }
   args(...args: string[]): this {
-    throw new Error('Method not implemented.');
+    this.#args.push(...args);
+    return this;
   }
   spawn(ffmpegPath: string = getFFmpegPath()): FFmpegProcess {
-    // throw new Error('Method not implemented.');
-    return new Process(ffmpegPath, this.getArgs(), null as any, null as any);
+    return new Process(ffmpegPath, this.getArgs(), null, null);
   }
   getArgs(): string[] {
+    const inputs = ([] as string[]).concat(...this.#inputs.map(i => i.getArgs()));
+    const outputs = ([] as string[]).concat(...this.#outputs.map(o => o.getArgs()));
     return [
       ...this.#args,
       '-v', this.logLevel.toString(),
+      ...inputs,
+      ...outputs,
     ];
   }
 }
@@ -123,16 +141,108 @@ class Process implements FFmpegProcess {
     });
   }
   progress(): AsyncGenerator<Progress, void, void> {
-    return progressGenerator(this.#process);
+    return progressGenerator(this.#process.stdout);
   }
   unwrap(): ChildProcess {
     return this.#process;
   }
 }
 
-async function* progressGenerator(process: ChildProcess) {
+const inputStreamMap = new WeakMap<Input, [string, NodeJS.ReadableStream]>();
+class Input implements FFmpegInput {
+  #resource: string;
+  #args: string[] = [];
+
+  isStream: boolean;
+  constructor(source: InputSource) {
+    if (typeof source === 'string') {
+      this.#resource = source;
+      this.isStream = false;
+    } else if (isBufferLike(source)) {
+      if (source.byteLength > MAX_BUFFER_LENGTH) {
+        const sockPath = getSockPath();
+        inputStreamMap.set(this, [sockPath, Readable.from([toUint8Array(source)])]);
+        this.#resource = sockPath;
+        this.isStream = true;
+      } else {
+        const buffer = Buffer.isBuffer(source) ? source : Buffer.from(source);
+        this.#resource = `data:application/octet-stream;base64,${buffer.toString('base64')}`;
+        this.isStream = false;
+      }
+    } else {
+      const sockPath = getSockPath();
+      inputStreamMap.set(this, [sockPath, 'pipe' in source ? source : Readable.from(source)]);
+      this.#resource = sockPath;
+      this.isStream = true;
+    }
+  }
+  getArgs(): string[] {
+    return [
+      ...this.#args,
+      '-i', this.#resource,
+    ];
+  }
+  args(...args: string[]): this {
+    this.#args.push(...args);
+    return this;
+  }
+}
+
+const outputStreamMap = new WeakMap<Output, [string, AsyncGenerator<void, void, Uint8Array>[]]>();
+class Output implements FFmpegOutput {
+  #resource: string;
+  #args: string[] = [];
+  isStream: boolean;
+
+  constructor(destinations: OutputDestination[]) {
+    if (destinations.length === 0) {
+      const sockPath = getSockPath();
+      outputStreamMap.set(this, [sockPath, []]);
+      this.#resource = sockPath;
+      this.isStream = true;
+    } else if (destinations.length === 1) {
+      const dest = destinations[0];
+      if (typeof dest === 'string') {
+        this.#resource = dest;
+        this.isStream = false;
+      } else {
+        const sockPath = getSockPath();
+        outputStreamMap.set(this, [sockPath, [toOutputStream(dest)]]);
+        this.#resource = sockPath;
+        this.isStream = true;
+      }
+    } else {
+      const resources: string[] = [];
+      const streams: AsyncGenerator<void, void, Uint8Array>[] = [];
+      const sockPath = getSockPath();
+      this.isStream = false;
+      for (const dest of destinations) {
+        if (typeof dest === 'string') {
+          resources.push(dest.replace(/[[|\]]/g, (char) => `\\${char}`));
+        } else {
+          if (!this.isStream) {
+            outputStreamMap.set(this, [sockPath, streams]);
+            resources.push(sockPath);
+            this.isStream = true;
+          }
+          streams.push(toOutputStream(dest));
+        }
+      }
+      this.#resource = `tee:${resources.join('|')}`;
+    }
+  }
+  args(...args: string[]): this {
+    this.#args.push(...args);
+    return this;
+  }
+  getArgs(): string[] {
+    return [...this.#args];
+  }
+}
+
+async function* progressGenerator(stream: NodeJS.ReadableStream) {
   let progress: Partial<Progress> = {};
-  for await (const line of readlines(process.stderr!)) {
+  for await (const line of readlines(stream)) {
     try {
       const [part1, part2] = line.split('=');
       const key = part1.trim();
@@ -191,4 +301,37 @@ function spawnProcess(ffmpegPath: string, args: string[], inputStream: NodeJS.Re
 
 async function pipeToGenerator(input: NodeJS.ReadableStream, output: AsyncGenerator<void, void, Uint8Array>) {
   for await (const chunk of input) await output.next(toUint8Array(chunk as Buffer));
+
+}
+
+function toOutputStream(stream: NodeJS.WritableStream | { [Symbol.asyncIterator](): AsyncIterator<any, any, Uint8Array>; } | { [Symbol.iterator](): Iterator<any, any, Uint8Array>; }): AsyncGenerator<void, void, Uint8Array> {
+  if ('write' in stream) {
+    return fromNodeStream(stream);
+  } else {
+    return fromIterable(stream);
+  }
+}
+
+async function* fromNodeStream(stream: NodeJS.WritableStream): AsyncGenerator<void, void, Uint8Array> {
+  try {
+    while (true) {
+      await write(stream, yield);
+    }
+  } finally {
+    await end(stream);
+  }
+}
+
+async function* fromIterable(stream: { [Symbol.asyncIterator](): AsyncIterator<any, any, Uint8Array>; } | { [Symbol.iterator](): Iterator<any, any, Uint8Array>; }): AsyncGenerator<void, void, Uint8Array> {
+  const it = __asyncValues(stream) as AsyncIterator<any, any, Uint8Array>;
+  let r: IteratorResult<any>, e, m;
+  try {
+    while (r = await it.next(yield), !r.done);
+  }
+  catch (error) { e = { error }; }
+  finally {
+    try { if (r! && !r!.done && (m = it.return)) m.call(it); }
+    // eslint-disable-next-line no-unsafe-finally
+    finally { if (e) throw e.error; }
+  }
 }
