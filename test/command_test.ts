313d93bb24af75c82fbbc6775f625f9369ef7afc
FedericoCarboni <fr3ddydev@gmail.com>
2020-09-28 16:47:10 +0200

    test: improving tests

--- test/command_test.ts
+++ test/command_test.ts
@@ -1,25 +1,13 @@
-import { expect } from 'chai';
+import { PassThrough, Readable } from 'stream';
+import { createReadStream, promises, unlinkSync } from 'fs';
 import { randomBytes } from 'crypto';
-import { Readable } from 'stream';
-// import { createReadStream, createWriteStream } from 'fs';
-import { ffmpeg } from '../src/command';
-// import { write } from '../src/utils';
-// import { read } from '../src/utils';
-// const writeStream = createWriteStream('video1.mp4');
-// const writeStream2 = createWriteStream('video2.mp4');
+import { expect } from 'chai';
 
-// async function* createNewWriteStream() {
-//   while (true) {
-//     const chunk = yield;
-//     console.log(chunk.length);
-//     await write(writeStream2, chunk)
-//       .then(() => { console.log('Written ', chunk.length); });
-//   }
-// }
+import { ffmpeg } from '../src/command';
 
 describe('command', function () {
-  this.timeout(60000);
   describe('FFmpegCommand', function () {
+    this.timeout(30000);
     describe('input()', function () {
       it('should add a string as source', function () {
         const cmd = ffmpeg();
@@ -29,9 +17,10 @@ describe('command', function () {
       });
       it('should add a short buffer as source', function () {
         const cmd = ffmpeg();
-        const input = cmd.input(randomBytes(4096));
-        expect(input.isStream).to.equal(false);
-        expect(input.getArgs().pop()!.startsWith('data:application/octet-stream;base64,')).to.equal(true);
+        const input1 = cmd.input(randomBytes(4096));
+        const input2 = cmd.input(randomBytes(4096).buffer);
+        expect(input1.isStream).to.equal(false);
+        expect(input2.isStream).to.equal(false);
       });
       it('should add a long buffer as source', function () {
         const cmd = ffmpeg();
@@ -78,7 +67,53 @@ describe('command', function () {
         expect(lastArg!.startsWith('tee:')).to.equal(true);
       });
     });
+    describe('args()', function () {
+      it('should return this', function () {
+        const cmd = ffmpeg();
+        expect(cmd.args()).to.equal(cmd);
+      });
+      it('should push arguments to the start of the command', async function () {
+        const cmd = ffmpeg();
+        expect(cmd.args()).to.equal(cmd);
+      });
+    });
     describe('spawn()', function () {
+      it('should handle a simple output destination', async function () {
+        try {
+          const cmd = ffmpeg();
+          cmd.input('test/samples/video.mp4');
+          cmd.output('test/samples/[strange]output.mkv')
+            .args('-c', 'copy', '-f', 'matroska');
+          const process = await cmd.spawn();
+          await process.wait();
+          expect((await promises.lstat('test/samples/[strange]output.mkv')).isFile()).to.equal(true);
+        } finally {
+          try {
+            unlinkSync('test/samples/[strange]output.mkv');
+          } catch {
+            //
+          }
+        }
+      });
+      it('should handle simple output destinations', async function () {
+        try {
+          const cmd = ffmpeg();
+          cmd.input('test/samples/video.mp4');
+          cmd.output('test/samples/output.mkv', 'test/samples/[strange]output.mkv')
+            .args('-c', 'copy', '-f', 'matroska');
+          const process = await cmd.spawn();
+          await process.wait();
+          expect((await promises.lstat('test/samples/[strange]output.mkv')).isFile()).to.equal(true);
+          expect((await promises.lstat('test/samples/output.mkv')).isFile()).to.equal(true);
+        } finally {
+          try {
+            unlinkSync('test/samples/[strange]output.mkv');
+            unlinkSync('test/samples/output.mkv');
+          } catch {
+            //
+          }
+        }
+      });
       it('should handle a single streaming output destination', async function () {
         async function* handleOutput() {
           while (true) {
@@ -105,7 +140,7 @@ describe('command', function () {
         }
         const cmd = ffmpeg();
         cmd.input('test/samples/video.mp4');
-        cmd.output(handleOutput1(), handleOutput2())
+        cmd.output(handleOutput1(), handleOutput2(), new PassThrough())
           .args('-c', 'copy', '-f', 'matroska');
         const process = await cmd.spawn();
         await process.wait();
@@ -126,6 +161,41 @@ describe('command', function () {
         cmd.output(handleOutput1())
           .args('-c', 'copy', '-f', 'matroska');
         cmd.output(handleOutput2())
+          .args('-c', 'copy', '-f', 'matroska');
+        cmd.output(new PassThrough())
+          .args('-c', 'copy', '-f', 'matroska');
+        const process = await cmd.spawn();
+        await process.wait();
+      });
+      it('should handle null outputs', async function () {
+        const cmd = ffmpeg();
+        cmd.input('test/samples/video.mp4');
+        cmd.output()
+          .args('-c', 'copy', '-f', 'matroska');
+        const process = await cmd.spawn();
+        await process.wait();
+      });
+      it('should handle a single streaming input source', async function () {
+        const cmd = ffmpeg();
+        cmd.input(createReadStream('test/samples/video.mp4'));
+        cmd.output()
+          .args('-c', 'copy', '-f', 'matroska');
+        const process = await cmd.spawn();
+        await process.wait();
+      });
+      it('should handle multiple streaming inputs', async function () {
+        const cmd = ffmpeg();
+        cmd.input(createReadStream('test/samples/video.mp4'));
+        cmd.input(createReadStream('test/samples/video.mkv'));
+        cmd.output()
+          .args('-map', '0:0', '-map', '1:1', '-c', 'copy', '-f', 'matroska');
+        const process = await cmd.spawn();
+        await process.wait();
+      });
+      it('should handle simple inputs', async function () {
+        const cmd = ffmpeg();
+        cmd.input('test/samples/video.mp4');
+        cmd.output()
           .args('-c', 'copy', '-f', 'matroska');
         const process = await cmd.spawn();
         await process.wait();
--- test/info_test.ts
+++ test/info_test.ts
@@ -1,9 +1,9 @@
-import { getCodecs, getDemuxers, getFormats, getMuxers, getVersion } from '../src/info';
+import { getCodecs, getDecoders, getDemuxers, getEncoders, getFilters, getFormats, getMuxers, getPixelFormats, getVersion } from '../src/info';
 import { expect } from 'chai';
 
 describe('info', function () {
   describe('getVersion()', function () {
-    it('returns information', async function () {
+    it('should return Version', async function () {
       const version = await getVersion();
       expect(version.copyright).to.be.a('string');
       expect(version.version).to.be.a('string');
@@ -19,25 +19,56 @@ describe('info', function () {
     });
   });
   describe('getMuxers()', function () {
-    it('returns a Set', async function () {
+    it('should return a Set', async function () {
       const muxers = await getMuxers();
       expect(muxers).to.be.an.instanceOf(Set);
     });
   });
   describe('getDemuxers()', function () {
-    it('returns a Set', async function () {
+    it('should return a Set', async function () {
       const demuxers = await getDemuxers();
       expect(demuxers).to.be.an.instanceOf(Set);
     });
   });
   describe('getFormats()', function () {
-    it('returns a Set', async function () {
+    it('should return a Set', async function () {
       const formats = await getFormats();
       expect(formats).to.be.an.instanceOf(Set);
     });
   });
+  describe('getEncoders()', function () {
+    it('should return Codecs', async function () {
+      const codecs = await getEncoders();
+      expect(codecs.video).to.be.an.instanceOf(Set);
+      expect(codecs.audio).to.be.an.instanceOf(Set);
+      expect(codecs.subtitle).to.be.an.instanceOf(Set);
+      expect(codecs.data).to.be.an.instanceOf(Set);
+    });
+  });
+  describe('getDecoders()', function () {
+    it('should return Codecs', async function () {
+      const codecs = await getDecoders();
+      expect(codecs.video).to.be.an.instanceOf(Set);
+      expect(codecs.audio).to.be.an.instanceOf(Set);
+      expect(codecs.subtitle).to.be.an.instanceOf(Set);
+      expect(codecs.data).to.be.an.instanceOf(Set);
+    });
+  });
+  describe('getPixelFormats()', function () {
+    it('should return a Set', async function () {
+      const pixelFormats = await getPixelFormats();
+      expect(pixelFormats).to.be.an.instanceOf(Set);
+    });
+  });
+  describe('getFilters()', async function () {
+    it('should return Filters', async function () {
+      const filters = await getFilters();
+      expect(filters.video).to.be.an.instanceOf(Set);
+      expect(filters.audio).to.be.an.instanceOf(Set);
+    });
+  });
   describe('getCodecs()', function () {
-    it('returns Codecs', async function () {
+    it('should return Codecs', async function () {
       const codecs = await getCodecs();
       expect(codecs.video).to.be.an.instanceOf(Set);
       expect(codecs.audio).to.be.an.instanceOf(Set);
