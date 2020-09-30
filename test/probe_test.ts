6dda26f995d541a049f3bccdf25daa5453409f5a
FedericoCarboni <fr3ddydev@gmail.com>
2020-09-30 17:00:42 +0200

    test: improve tests for probe.ts

--- test/probe_test.ts
+++ test/probe_test.ts
@@ -1,15 +1,30 @@
-import { ProbeResult } from '../src/probe/result';
-import { probe } from '../src/probe/mod';
-import { expect } from 'chai';
+import { probeUint8Array } from '../src/probe';
 import { join } from 'path';
 import { promises } from 'fs';
+import { expect } from 'chai';
+import { randomBytes } from 'crypto';
 
 describe('probe', function () {
   describe('probe()', function () {
-    it('should return an instance of ProbeResult', async function () {
+    it('should return a ProbeResult', async function () {
       const buffer = await promises.readFile(join(__dirname, 'samples/video.mp4'));
-      const result = await probe(buffer);
-      expect(result).to.be.an.instanceOf(ProbeResult);
+      const result = await probeUint8Array(buffer);
+      expect(result.format).to.equal('mov,mp4,m4a,3gp,3g2,mj2');
+      expect(result.duration).to.be.a('number');
+      expect(result.start).to.be.a('number');
+      expect(result.score).to.equal(100);
+      expect(result.tags).to.be.an.instanceOf(Map);
+      expect(result.unwrap()).to.be.an('object');
+    });
+    it('should throw on invalid input', async function () {
+      const buffer = randomBytes(6 * 1024 * 1024);
+      let caught = false;
+      try {
+        await probeUint8Array(buffer);
+      } catch {
+        caught = true;
+      }
+      expect(caught).to.equal(true);
     });
   });
 });
