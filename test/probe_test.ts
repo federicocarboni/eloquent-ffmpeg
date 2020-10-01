273b12ebabe83bd28ba378fa0d4eb9e568b4f2a2
FedericoCarboni <fr3ddydev@gmail.com>
2020-10-01 19:04:36 +0200

    test: update tests for probe()

--- test/probe_test.ts
+++ test/probe_test.ts
@@ -1,14 +1,31 @@
-import { probe } from '../src/probe';
-import { join } from 'path';
-import { promises } from 'fs';
-import { expect } from 'chai';
+import { createReadStream, promises } from 'fs';
 import { randomBytes } from 'crypto';
+import { probe } from '../src/probe';
+import { expect } from 'chai';
 
 describe('probe', function () {
   describe('probe()', function () {
-    it('should return a ProbeResult', async function () {
-      const buffer = await promises.readFile(join(__dirname, 'samples/video.mp4'));
+    it('should probe a buffer', async function () {
+      const buffer = await promises.readFile('test/samples/video.mp4');
       const result = await probe(buffer);
+      expect(result.format).to.equal('mov,mp4,m4a,3gp,3g2,mj2');
+      expect(result.duration).to.be.a('number');
+      expect(result.start).to.be.a('number');
+      expect(result.score).to.equal(100);
+      expect(result.tags).to.be.an.instanceOf(Map);
+      expect(result.unwrap()).to.be.an('object');
+    });
+    it('should probe a readable stream', async function () {
+      const result = await probe(createReadStream('test/samples/video.mp4'));
+      expect(result.format).to.equal('mov,mp4,m4a,3gp,3g2,mj2');
+      expect(result.duration).to.be.a('number');
+      expect(result.start).to.be.a('number');
+      expect(result.score).to.equal(100);
+      expect(result.tags).to.be.an.instanceOf(Map);
+      expect(result.unwrap()).to.be.an('object');
+    });
+    it('should probe a file path', async function () {
+      const result = await probe('test/samples/video.mp4');
       expect(result.format).to.equal('mov,mp4,m4a,3gp,3g2,mj2');
       expect(result.duration).to.be.a('number');
       expect(result.start).to.be.a('number');
