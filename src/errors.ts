779b644ee819406c1fdf24d888e45fae8f90f9c4
FedericoCarboni <fr3ddydev@gmail.com>
2020-10-04 17:41:52 +0200

    feat: added errors.ts

---
+++ src/errors.ts
@@ -0,0 +1,18 @@
+export class FFmpegError extends Error {
+  constructor(message: string, public stderr: readonly string[]) {
+    super(message);
+  }
+}
+
+export function extractMessage(stderr: string[]): string | undefined {
+  let message: string | undefined;
+  for (const line of stderr) {
+    if (line !== '') {
+      if (!message && line.includes(': '))
+        message = line.slice(line.indexOf(': ') + 2);
+      if (line.startsWith('[NULL @ '))
+        message = line.slice(line.indexOf('] ') + 2);
+    }
+  }
+  return message;
+}
