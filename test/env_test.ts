7b39fc06d288be66f30ef9e1150b0d5e5472dbf9
FedericoCarboni <fr3ddydev@gmail.com>
2020-10-05 12:09:58 +0200

    fix: inconsistent descriptions

--- test/env_test.ts
+++ test/env_test.ts
@@ -3,23 +3,23 @@ import { expect } from 'chai';
 
 describe('env', function () {
   describe('setFFmpegPath()', function () {
-    it('throws on an invalid path', function () {
+    it('should throw on an invalid path', function () {
       expect(() => setFFmpegPath('broken/:path')).to.throw(TypeError);
     });
   });
   describe('setFFprobePath()', function () {
-    it('throws on an invalid path', function () {
+    it('should throw on an invalid path', function () {
       expect(() => setFFprobePath('broken/:path')).to.throw(TypeError);
     });
   });
 
   describe('getFFmpegPath()', function () {
-    it('returns a string', function () {
+    it('should return a string', function () {
       expect(getFFmpegPath()).to.be.a('string');
     });
   });
   describe('getFFprobePath()', function () {
-    it('returns a string', function () {
+    it('should return a string', function () {
       expect(getFFprobePath()).to.be.a('string');
     });
   });
