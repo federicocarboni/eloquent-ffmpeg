f5935ed1c332fc464f69fe83c1cce908d1b1a3e9
FedericoCarboni <fr3ddydev@gmail.com>
2020-10-14 21:44:21 +0200

    build: update rollup configuration
    Replace require() with createRequire()() in ESM build. Dynamic require used for ntsuspend needed for #1

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import cleanup from 'rollup-plugin-cleanup';
+import inject from '@rollup/plugin-inject';
 import { execSync } from 'child_process';
+import * as path from 'path';
 import pkg from './package.json';
 
 const commit = execSync('git rev-parse HEAD').toString('utf-8').slice(0, -1);
@@ -14,6 +16,19 @@ const banner = `/**
  * @author ${pkg.author.name}
  */`;
 
+const external = ['child_process', 'crypto', 'readline', 'stream', 'net', 'path', 'fs'];
+const plugins = [
+  typescript({
+    tsconfig: './tsconfig.es6.json'
+  }),
+  resolve(),
+  cleanup({
+    comments: 'none',
+    include: ['src/**/*.ts', 'node_modules/**'],
+    extensions: ['ts', 'js'],
+  }),
+];
+
 export default [{
   input: 'src/lib.ts',
   output: [{
@@ -21,20 +36,24 @@ export default [{
     format: 'es',
     dir: 'lib',
     banner,
-  }, {
+  }],
+  plugins: [
+    ...plugins,
+    inject({
+      modules: {
+        require: [path.resolve('src/require.ts'), 'require']
+      }
+    })
+  ],
+  external,
+}, {
+  input: 'src/lib.ts',
+  output: [{
     entryFileNames: 'lib.cjs',
     format: 'cjs',
     dir: 'lib',
     banner,
   }],
-  plugins: [
-    typescript({ tsconfig: './tsconfig.es6.json' }),
-    resolve(),
-    cleanup({
-      comments: 'none',
-      include: ['src/**/*.ts', 'node_modules/**'],
-      extensions: ['ts', 'js'],
-    }),
-  ],
-  external: ['child_process', 'crypto', 'readline', 'stream', 'net', 'path', 'fs'],
+  plugins,
+  external,
 }];
