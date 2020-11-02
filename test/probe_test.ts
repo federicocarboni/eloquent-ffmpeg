1aabd5f1f12bb0a8b30777d670acbf8e2ac2530e
FedericoCarboni <fr3ddydev@gmail.com>
2020-11-02 15:41:26 +0100

    test: improve code coverage for probe.ts

--- test/probe_test.ts
+++ test/probe_test.ts
@@ -24,13 +24,33 @@ describe('probe', function () {
       expect(result.tags).to.be.an.instanceOf(Map);
       expect(result.unwrap()).to.be.an('object');
     });
-    it('should probe a file path', async function () {
+    it('should probe a file by path', async function () {
       const result = await probe('test/samples/video.mp4');
       expect(result.format).to.equal('mov,mp4,m4a,3gp,3g2,mj2');
       expect(result.duration).to.be.a('number');
       expect(result.start).to.be.a('number');
       expect(result.score).to.equal(100);
       expect(result.tags).to.be.an.instanceOf(Map);
+      expect(result.unwrap()).to.be.an('object');
+    });
+    it('should probe a custom analyzeDuration', async function () {
+      const result = await probe('test/samples/video.mp4', {
+        analyzeDuration: 30000,
+      });
+      expect(result.format).to.equal('mov,mp4,m4a,3gp,3g2,mj2');
+      expect(result.duration).to.be.a('number');
+      expect(result.start).to.be.a('number');
+      expect(result.score).to.equal(100);
+      expect(result.tags).to.be.an.instanceOf(Map);
+      expect(result.unwrap()).to.be.an('object');
+    });
+    it('should probe an image stream', async function () {
+      const result = await probe(createReadStream('test/samples/image.webp'), {
+        format: 'image2pipe',
+      });
+      expect(result.format).to.equal('image2pipe');
+      expect(result.duration).to.be.a('number');
+      expect(result.start).to.be.a('number');
       expect(result.unwrap()).to.be.an('object');
     });
     it('should throw on invalid input path', async function () {
