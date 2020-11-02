2d46ed510fdae5240426ca1e933b1eda693d686f
FedericoCarboni <fr3ddydev@gmail.com>
2020-11-02 15:27:16 +0100

    test: improve code coverage

--- test/env_test.ts
+++ test/env_test.ts
@@ -6,10 +6,20 @@ describe('env', function () {
     it('should throw on an invalid path', function () {
       expect(() => setFFmpegPath('broken/:path')).to.throw(TypeError);
     });
+    it('should accept a valid path', function () {
+      const path = getFFmpegPath();
+      expect(() => setFFmpegPath(__filename)).not.to.throw();
+      setFFmpegPath(path);
+    });
   });
   describe('setFFprobePath()', function () {
     it('should throw on an invalid path', function () {
       expect(() => setFFprobePath('broken/:path')).to.throw(TypeError);
+    });
+    it('should accept a valid path', function () {
+      const path = getFFprobePath();
+      expect(() => setFFprobePath(__filename)).not.to.throw();
+      setFFprobePath(path);
     });
   });
 
