2f963ddaf21b0250423c4c81388d604e29f67655
FedericoCarboni <fr3ddydev@gmail.com>
2020-11-06 16:24:24 +0100

    fix: clean up helper code

--- src/utils.ts
+++ src/utils.ts
@@ -5,12 +5,36 @@ export const isWin32 = process.platform === 'win32';
 
 export const IGNORED_ERRORS = new Set(['ECONNRESET', 'EPIPE', 'EOF']);
 
-export function isNullish(o: unknown): o is undefined | null {
-  return o === void 0 || o === null;
-}
+export const isNullish = (o: unknown): o is undefined | null => o === void 0 || o === null;
 
-export function read(stream: NodeJS.ReadableStream): Promise<Buffer> {
-  return new Promise((resolve, reject) => {
+export const isObject = (o: unknown): o is any => o !== null && typeof o === 'object';
+
+const isStream = (o: unknown): o is any => isObject(o) && typeof o.pipe === 'function';
+
+export const isReadableStream = (o: unknown): o is NodeJS.ReadableStream => {
+  try {
+    return isStream(o) &&
+      'readable' in o && o.readable !== false &&
+      typeof o._read === 'function' &&
+      typeof o._readableState === 'object';
+  } catch {
+    return false;
+  }
+};
+
+export const isWritableStream = (o: unknown): o is NodeJS.WritableStream => {
+  try {
+    return isStream(o) &&
+      'writable' in o && o.writable !== false &&
+      typeof o._write === 'function' &&
+      typeof o._writableState === 'object';
+  } catch {
+    return false;
+  }
+};
+
+export const read = (stream: NodeJS.ReadableStream): Promise<Buffer> =>
+  new Promise((resolve, reject) => {
     const chunks: Uint8Array[] = [];
     const unlisten = (): void => {
       stream.off('readable', onReadable);
@@ -37,23 +61,20 @@ export function read(stream: NodeJS.ReadableStream): Promise<Buffer> {
     stream.on('error', onError);
     stream.resume();
   });
-}
 
-export function write(stream: NodeJS.WritableStream, chunk?: any): Promise<void> {
-  return new Promise((resolve, reject) => {
-    stream.write(chunk, () => {
+export const write = (stream: NodeJS.WritableStream, chunk: Uint8Array): Promise<void> =>
+  new Promise((resolve, reject) => {
+    stream.write(chunk as any, () => {
       stream.off('error', reject);
       resolve();
     });
     stream.once('error', reject);
   });
-}
 
-export function toReadable(source: Uint8Array | AsyncIterable<Uint8Array>): NodeJS.ReadableStream {
-  return 'readable' in source ? source : Readable.from(
+export const toReadable = (source: Uint8Array | AsyncIterable<Uint8Array>): NodeJS.ReadableStream =>
+  isReadableStream(source) ? source : Readable.from(
     source instanceof Uint8Array ? [source] : source, { objectMode: false }
   );
-}
 
 // Node.js <11 doesn't support `Array.prototype.flatMap()`, this uses `flatMap`
 // if available or falls back to using `Array.prototype.map` and
