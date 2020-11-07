1e9399dfff7372866bce5b97bb1bccc17168fa1a
FedericoCarboni <fr3ddydev@gmail.com>
2020-11-07 12:52:11 +0100

    style: update code style

--- src/sock.ts
+++ src/sock.ts
@@ -3,18 +3,18 @@ import { isWin32 } from './utils';
 import { v4 } from 'uuid';
 
 /* istanbul ignore next */
-export const getSocketPath = isWin32 ? (): string => {
-  return `//./pipe/ffmpeg-ipc-${v4()}.sock`;
-} : (): string => {
-  return `/tmp/ffmpeg-ipc-${v4()}.sock`;
-};
+export const getSocketPath = isWin32 ? (): string => (
+  `//./pipe/ffmpeg-ipc-${v4()}.sock`
+) : (): string => (
+  `/tmp/ffmpeg-ipc-${v4()}.sock`
+);
 
 /* istanbul ignore next */
-export const getSocketUrl = isWin32 ? (path: string): string => {
-  return `file:${path}`;
-} : (path: string): string => {
-  return `unix:${path}`;
-};
+export const getSocketUrl = isWin32 ? (path: string): string => (
+  `file:${path}`
+) : (path: string): string => (
+  `unix:${path}`
+);
 
 export function createSocketServer(path: string): Promise<Server> {
   return new Promise((resolve) => {
