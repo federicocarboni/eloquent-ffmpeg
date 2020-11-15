36018afef8001d0805e2e99cd03ee947004dfbd3
FedericoCarboni <fr3ddydev@gmail.com>
2020-11-15 10:48:04 +0100

    test: setup config for jest

---
+++ jest.config.js
@@ -0,0 +1,54 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  // collectCoverageFrom: undefined,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: [
+    'json',
+    'text',
+    'lcov'
+  ],
+
+  // A preset that is used as a base for Jest's configuration
+  preset: 'ts-jest',
+
+  // The paths to modules that run some code to configure or set up the testing environment before each test
+  setupFiles: ['dotenv/config'],
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  // setupFilesAfterEnv: [],
+
+  // The number of seconds after which a test is considered as slow and reported as such in the results.
+  slowTestThreshold: 10,
+
+  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
+  // snapshotSerializers: [],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    '**/test/**/*.[jt]s?(x)'
+  ],
+
+  // Indicates whether each individual test should be reported during the run
+  verbose: true,
+};
